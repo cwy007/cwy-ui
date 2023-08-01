d139c4d470950ac5b1a9372e2d55434fb33628ee
chenweiyan <chanweiyan007@gmail.com>
2023-08-01 11:40:56 +0800

    fix: update social link

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,5 +4,8 @@ export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'cwy-ui',
+    socialLinks: {
+      github: 'https://github.com/cwy007/cwy-ui',
+    },
   },
 });
